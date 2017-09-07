234033b6c401afe58ca4edadd02310f26e9d6024
Anthony <anthony.caccavella@gmail.com>
2017-09-07 15:26:21 -0600

    Finished build, but login button not working

--- fullstackapp/src/components/Private/Private.js
+++ fullstackapp/src/components/Private/Private.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import './Private.css';
+import {connect} from 'react-redux';
+import {getUserInfo} from '../../ducks/user_reducer'
 
-export default class Private extends Component{
+class Private extends Component{
     render() {
+        console.log(this.props.user);
         return(
             <div>
                 <h1>Private</h1>
+                <h2>This is a test</h2>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+let outputActions = {
+    getUserInfo: getUserInfo
+}
+
+export default connect(mapStateToProps, outputActions)(Private);
--- fullstackapp/src/index.js
+++ fullstackapp/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {HashRouter as Router} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-<Router>
-<App />
-</Router>
-, document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
